13d5e3a0e736219667bd3303fdddde028d77a310
skanderkaroui <skander.karoui@gmail.com>
2024-08-11 13:42:27 +0100

    calling retrieval for answer

--- RAG/main.py
+++ RAG/main.py
@@ -1,15 +1,19 @@
+import uvicorn
 from fastapi import FastAPI, HTTPException
 from RAG.models import ChatRequest
 from RAG.qa_chain import get_qa_chain
 from RAG.utils import generate_analysis_and_tips
+from RAG.retrieval import retrieval
+
 
 app = FastAPI()
+
 qa_chain = get_qa_chain()
 
 @app.post("/chat")
 async def chat(request: ChatRequest):
     try:
-        answer = qa_chain.run(request.user_input)
+        answer = retrieval(request.user_input)
         analysis, tips = generate_analysis_and_tips(request.user_input)
         return {
             "question_analysis": analysis,
@@ -18,3 +22,6 @@ async def chat(request: ChatRequest):
         }
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+if __name__ == "__main__":
+    uvicorn.run("RAG.main:app", host="127.0.0.1", port=8000, reload=True)
